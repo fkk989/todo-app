d5e8a1b1da20b5d48220abb5d4d45fd92505bb2a
faisal khan <faisalkhan@faisals-MacBook-Air.local>
2023-08-07 15:33:12 +0530

    ignored some file

--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,1 @@
-ssh -t -i "cert-file.cer" ubuntu@ec2-url  "sudo bash ~/deploy.sh"
+ssh -t -i "todo-app-class-2.pem" ubuntu@ec2-13-234-238-231.ap-south-1.compute.amazonaws.com  "sudo bash ~/deploy.sh"
